0005225571afe95191058126e718fead6fc501f1
@e.pichugin <egor.pichugin@enterar.com>
2024-11-27 22:09:16 +0100

    removed cashed files

--- Excercise1/main.py
+++
@@ -1,8 +0,0 @@
-from ParserScript.Parsers.DataImporter import DataImporter
-
-importer: DataImporter = DataImporter()
-importer.ConnectToDatabase()
-importer.Parse()
-importer.InitializeServices()
-importer.ImportData()
-importer.CloseConnection()
